09662944abec3ae6e19925883667e97f032f3729
Tim Gabriel <tgabriel@nebrwesleyan.edu>
2024-04-14 21:33:30 -0500

    Average Daily Tone grouped by country as determined by URL domain

--- mappingProject.R
+++ mappingProject.R
@@ -4,21 +4,23 @@ library(urltools)
 library(readxl)
 
 # Gather all mentions of an event.
-apr7 <- GetGDELT("2024-04-07")
+apr13 <- GetGDELT("2024-04-13")
 countryDomains <- read_excel("countryDomains.xlsx")
 
-apr7 %>% group_by(SOURCEURL, ActionGeo_Lat, ActionGeo_Long, EventRootCode) %>%
+apr13 %>% group_by(SOURCEURL, ActionGeo_Lat, ActionGeo_Long, EventRootCode) %>%
   summarise(n(),mean(AvgTone)) %>%
   mutate(domain = (domain(SOURCEURL))) -> df1
 
 df1 %>% ungroup() %>% select(`n()`, domain, `mean(AvgTone)`) %>% 
   mutate(suffix = sub(".+\\.", ".", domain)) %>%
-  left_join(countryDomains, by = c("suffix"="domain")) %>%
-  na.omit() -> df2
+  left_join(countryDomains, by = c("suffix"="domain")) -> df2
+df2 %>% print(n=100)  
+df2 %>% na.omit() -> df3
+df3 %>% print(n=100)
 
 # Josh's work with MentionSourceName (or however he is getting the URL)
 # will allow us to sort these mentions by their country of origin.
-df2 %>% group_by(country) %>% summarise(mean(`mean(AvgTone)`))
+df2 %>% group_by(country) %>% summarise(mean(`mean(AvgTone)`)) %>% print(n=92)
 
 
 # For each country, we can average the values of MentionDocTone
